0363c6b8c4a3d163ddef3c1a8be09d1075dbcb18
Dmitry <jinrlit@gmail.com>
2019-04-01 10:59:08 +0300

    Итоговая версия перед презентацией

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, DateField, SelectField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username')
     password = PasswordField('Password')
     submit = SubmitField('Sign In')
+
+class Order(FlaskForm):
+    client = StringField("Заказчик")
+    product = StringField("Изделие")
+    count = StringField("Количество")
+    date = StringField("Желаемый срок")
+    price = StringField("Цена")
+    priority = SelectField("Приориет",
+                           choices=[(1, 'Высокий'),
+                                    (2, 'Общий'),
+                                    (3, 'Низкий')],
+                           default=2)
+    submit = SubmitField('Подтвердить')
